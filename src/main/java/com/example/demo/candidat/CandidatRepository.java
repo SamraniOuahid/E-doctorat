fdef6a980d94b1e22cde7a40b20ea905e5da2232
ouahidsamrani <ouahid.samrani@usmba.ac.ma>
2025-12-07 15:10:24 +0000

    delete file

--- src/main/java/com/example/demo/candidat/CandidatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.candidat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidatRepository extends JpaRepository <CandidatModel, Integer>{
-
-}
